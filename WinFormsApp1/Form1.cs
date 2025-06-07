4a675612b7a7133ee68698cfb4e29a2c66ac3dac
kesigomon@gmail.com <34025522+kesigomon@users.noreply.github.com>
2025-06-08 02:07:23 +0900

    別離

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -12,9 +12,9 @@ namespace WinFormsApp1;
 public partial class Form1 : Form, IWinFormsShell
 {
     private readonly OpenIddictClientService _service;
-    #nullable enable
+#nullable enable
     private HubConnection? _connection;
-    #nullable disable
+#nullable disable
     private string _token = "";
     private string _refreshToken = "";
 
@@ -31,92 +31,98 @@ public partial class Form1 : Form, IWinFormsShell
 
         try
         {
-            using var source = new CancellationTokenSource(delay: TimeSpan.FromSeconds(90));
-
-            try
-            {
-                // Ask OpenIddict to initiate the authentication flow (typically, by starting the system browser).
-                var result = await _service.ChallengeInteractivelyAsync(new()
-                {
-                    CancellationToken = source.Token,
-                    Scopes = [
-                        OpenIddictConstants.Scopes.OfflineAccess
-                    ]
-                });
-
-                // Wait for the user to complete the authorization process.
-                var resultAuth = await _service.AuthenticateInteractivelyAsync(new()
-                {
-                    CancellationToken = source.Token,
-                    Nonce = result.Nonce
-                });
-                _token = resultAuth.BackchannelAccessToken;
-                _refreshToken = resultAuth.RefreshToken;
-                await ExampleSignalR(source.Token);
-                TaskDialog.ShowDialog(new()
-                {
-                    Caption = "Authentication successful",
-                    Heading = "Authentication successful",
-                    Icon = TaskDialogIcon.ShieldSuccessGreenBar,
-                    Text = $"Authentication successful. Token is hidden."
-                });
-            }
-
-            catch (OperationCanceledException)
-            {
-                // タイムアウト
-                TaskDialog.ShowDialog(new TaskDialogPage
-                {
-                    Caption = "Authentication timed out",
-                    Heading = "Authentication timed out",
-                    Icon = TaskDialogIcon.Warning,
-                    Text = "The authentication process was aborted."
-                });
-            }
-
-            catch (OpenIddictExceptions.ProtocolException exception) when (exception.Error ==
-                                                                           OpenIddictConstants.Errors.UnauthorizedClient)
-            {
-                // ログインしたユーザーがサーバーにいないか、入鋏ロールがついてない
-                TaskDialog.ShowDialog(new TaskDialogPage
-                {
-                    Caption = "Authorization denied",
-                    Heading = "Authorization denied",
-                    Icon = TaskDialogIcon.Warning,
-                    Text = "The authorization was denied by the end user."
-                });
-            }
-            catch (OpenIddictExceptions.ProtocolException exception) when (exception.Error ==
-                                                                           OpenIddictConstants.Errors.ServerError)
-            {
-                // サーバーでトラブル発生
-                TaskDialog.ShowDialog(new TaskDialogPage
-                {
-                    Caption = "Invalid request",
-                    Heading = "Invalid request",
-                    Icon = TaskDialogIcon.Warning,
-                    Text = "The authentication request was invalid."
-                });
-            }
-            catch (Exception exception)
-            {
-                // 予期しないエラー
-                Debug.WriteLine("An unexpected error occurred during authentication: " + exception);
-                Debug.WriteLine(exception.StackTrace);
-                TaskDialog.ShowDialog(new TaskDialogPage
-                {
-                    Caption = "Authentication failed",
-                    Heading = "Authentication failed",
-                    Icon = TaskDialogIcon.Error,
-                    Text = "An error occurred while trying to authenticate the user."
-                });
-            }
+            await InteractiveAuthenticateAsync(CancellationToken.None);
         }
 
         finally
         {
             // Re-enable the login button to allow starting a new authentication operation.
             LoginButton.Enabled = true;
+        }
+    }
+
+    private async Task InteractiveAuthenticateAsync(CancellationToken cancellationToken)
+    {
+        using var source = new CancellationTokenSource(delay: TimeSpan.FromSeconds(90));
+
+        try
+        {
+            // Ask OpenIddict to initiate the authentication flow (typically, by starting the system browser).
+            var result = await _service.ChallengeInteractivelyAsync(new()
+            {
+                CancellationToken = source.Token,
+                Scopes =
+                [
+                    OpenIddictConstants.Scopes.OfflineAccess
+                ]
+            });
+
+            // Wait for the user to complete the authorization process.
+            var resultAuth = await _service.AuthenticateInteractivelyAsync(new()
+            {
+                CancellationToken = source.Token,
+                Nonce = result.Nonce
+            });
+            _token = resultAuth.BackchannelAccessToken;
+            _refreshToken = resultAuth.RefreshToken;
+            await ExampleSignalR(source.Token);
+            TaskDialog.ShowDialog(new()
+            {
+                Caption = "Authentication successful",
+                Heading = "Authentication successful",
+                Icon = TaskDialogIcon.ShieldSuccessGreenBar,
+                Text = $"Authentication successful. Token is hidden."
+            });
+        }
+
+        catch (OperationCanceledException)
+        {
+            // タイムアウト
+            TaskDialog.ShowDialog(new TaskDialogPage
+            {
+                Caption = "Authentication timed out",
+                Heading = "Authentication timed out",
+                Icon = TaskDialogIcon.Warning,
+                Text = "The authentication process was aborted."
+            });
+        }
+
+        catch (OpenIddictExceptions.ProtocolException exception) when (exception.Error ==
+                                                                       OpenIddictConstants.Errors.UnauthorizedClient)
+        {
+            // ログインしたユーザーがサーバーにいないか、入鋏ロールがついてない
+            TaskDialog.ShowDialog(new TaskDialogPage
+            {
+                Caption = "Authorization denied",
+                Heading = "Authorization denied",
+                Icon = TaskDialogIcon.Warning,
+                Text = "The authorization was denied by the end user."
+            });
+        }
+        catch (OpenIddictExceptions.ProtocolException exception) when (exception.Error ==
+                                                                       OpenIddictConstants.Errors.ServerError)
+        {
+            // サーバーでトラブル発生
+            TaskDialog.ShowDialog(new TaskDialogPage
+            {
+                Caption = "Invalid request",
+                Heading = "Invalid request",
+                Icon = TaskDialogIcon.Warning,
+                Text = "The authentication request was invalid."
+            });
+        }
+        catch (Exception exception)
+        {
+            // 予期しないエラー
+            Debug.WriteLine("An unexpected error occurred during authentication: " + exception);
+            Debug.WriteLine(exception.StackTrace);
+            TaskDialog.ShowDialog(new TaskDialogPage
+            {
+                Caption = "Authentication failed",
+                Heading = "Authentication failed",
+                Icon = TaskDialogIcon.Error,
+                Text = "An error occurred while trying to authenticate the user."
+            });
         }
     }
 
@@ -166,6 +172,7 @@ public partial class Form1 : Form, IWinFormsShell
                 {
                     return;
                 }
+
                 Debug.WriteLine($"Error: {error.Message} {error.StackTrace}");
                 var cancellationToken = new CancellationTokenSource(TimeSpan.FromSeconds(90)).Token;
                 await RefreshTokenAsync(cancellationToken);
